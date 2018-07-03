5ae208b417e6b77948a150cbbb7c62ca24760205
Ilia Kasianenko <i.kasianenko@gmail.com>
2018-07-03 22:09:24 +0300

    add Bitmap::Data public interface skeleton

---
+++ lib/bitmap/data.rb
@@ -0,0 +1,22 @@
+module Bitmap
+  class Data
+
+    def initialize(w, h)
+    end
+
+    def clear
+    end
+
+    def set_colour(x, y, c)
+    end
+
+    def draw_vertical_line(x, y1, y2, c)
+    end
+
+    def draw_horizontal_line(x1, x2, y, c)
+    end
+
+    def show
+    end
+  end
+end
---
+++ spec/lib/bitmap/data_spec.rb
@@ -0,0 +1,21 @@
+require './lib/bitmap/data'
+
+RSpec.describe Bitmap::Data do
+  xdescribe '.new' do
+  end
+
+  xdescribe '.clear' do
+  end
+
+  xdescribe '.set_colour' do
+  end
+
+  xdescribe '.draw_vertical_line' do
+  end
+
+  xdescribe '.draw_horizontal_line' do
+  end
+
+  xdescribe '.show' do
+  end
+end
