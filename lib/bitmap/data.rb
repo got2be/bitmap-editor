f2bd8881148feafbbe02eba98884e3c50074ec75
Ilia Kasianenko <i.kasianenko@gmail.com>
2018-07-04 11:20:34 +0300

    implemented image creation

--- lib/bitmap/data.rb
+++ lib/bitmap/data.rb
@@ -1,7 +1,17 @@
 module Bitmap
   class Data
+    DEFAULT_COLOUR = 'O'.freeze
+    VALID_SIZE_RANGE = (1..250)
+
+    attr_accessor :width, :height, :data
 
     def initialize(w, h)
+      @width = w.to_i
+      @height = h.to_i
+
+      validate_size
+
+      @data = @data = Array.new(height) { Array.new(width, DEFAULT_COLOUR) }
     end
 
     def clear
@@ -17,6 +27,13 @@ module Bitmap
     end
 
     def show
+    end
+
+    private
+
+    def validate_size
+      return if VALID_SIZE_RANGE.include?(width) && VALID_SIZE_RANGE.include?(height)
+      raise "Image size #{[width, height]} is beyond the scope #{VALID_SIZE_RANGE}."
     end
   end
 end
--- spec/lib/bitmap/data_spec.rb
+++ spec/lib/bitmap/data_spec.rb
@@ -1,7 +1,52 @@
 require './lib/bitmap/data'
 
 RSpec.describe Bitmap::Data do
-  xdescribe '.new' do
+  describe '.new' do
+    let(:max_size) { 250 }
+    let(:width) { Random.rand(max_size) + 1 }
+    let(:height) { Random.rand(max_size) + 1 }
+    let(:default_colour) { 'O' }
+
+    subject { described_class.new(width, height) }
+
+    context 'args are not ok' do
+      shared_examples_for 'invalid args' do
+        it 'raises an error' do
+          expect { subject }.to raise_error("Image size #{[width, height]} is beyond the scope 1..#{max_size}.")
+        end
+      end
+
+      context 'width is too large' do
+        let(:width) { max_size + 1 }
+        it_behaves_like 'invalid args'
+      end
+
+      context 'width is too small' do
+        let(:width) { 0 }
+        it_behaves_like 'invalid args'
+      end
+
+      context 'height is too large' do
+        let(:height) { max_size + 1 }
+        it_behaves_like 'invalid args'
+      end
+
+      context 'height is too small' do
+        let(:height) { 0 }
+        it_behaves_like 'invalid args'
+      end
+    end
+
+    context 'args are ok' do
+      it 'creates image of specified size' do
+        expect(subject.data.length).to eq(height)
+        expect(subject.data.map(&:length).uniq).to eq([width])
+      end
+
+      it 'fills image with default colour' do
+        expect(subject.data.map(&:join).join).to eq(default_colour * width * height)
+      end
+    end
   end
 
   xdescribe '.clear' do
